5198549a246795aa7c8f1d160ae9fc3d3e3e626e
Dalton <dalton@MBP1702.local>
2022-10-24 10:12:21 -0400

    updated JoplinServerCopy user account

--- launchd/JoplinServerCopy/JoplinServerCopy.sh
+++ launchd/JoplinServerCopy/JoplinServerCopy.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
-PASSWORD=`security find-generic-password -s "BasementUser" -a "user" -w`
+PASSWORD=`security find-generic-password -s "VaultUser" -a "dalton" -w`
 
-[ ! -d "/Volumes/AdminExecs" ] && open -g -j smb://user:$PASSWORD@basement/AdminExecs
+[ ! -d "/Volumes/AdminExecs" ] && open -g -j smb://dalton:$PASSWORD@vault/AdminExecs
 
 rsync -avhHP "/Volumes/AdminExecs/Systems Admin/Sys Docs/Joplin" "/Users/Dalton/Documents/Documentation"
