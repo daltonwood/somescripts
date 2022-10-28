8c77078a1881b1798b4f65e405aed9352cdd762d
Dalton <dalton@MBP1702.local>
2022-10-28 10:13:55 -0400

    corrected paths in JoplinServerCopy.sh

--- launchd/JoplinServerCopy/JoplinServerCopy.sh
+++ launchd/JoplinServerCopy/JoplinServerCopy.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 PASSWORD=`security find-generic-password -s "VaultUser" -a "dalton" -w`
 
-[ ! -d "/Volumes/AdminExecs" ] && open -g -j smb://dalton:$PASSWORD@vault/AdminExecs
+[ ! -d "/Volumes/Cave/AdminExecs" ] && open -g -j smb://dalton:$PASSWORD@vault/Cave/AdminExecs
 
-rsync -avhHP "/Volumes/AdminExecs/Systems Admin/Sys Docs/Joplin" "/Users/Dalton/Documents/Documentation"
+rsync -avhHP "/Volumes/Cave/AdminExecs/SysAdmin/Sys Docs/Joplin" "/Users/Dalton/Documents/Documentation"
